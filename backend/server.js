56028e33cc40e787e197080a110dc813623b306a
Hao MIN <bennymin.1989@gmail.com>
2020-08-19 23:34:30 +0200

    Add backend coding

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const database = {
     users: [
@@ -22,6 +25,14 @@ const database = {
             entries: 0,
             joined: new Date()
         }
+    ],
+
+    login: [
+        {
+            id: "987",
+            hash: '',
+            email: 'john@gmail.com'
+        }
     ]
 }
 
@@ -29,10 +40,42 @@ app.get('/', (req, res) => {
     res.send(database.users);
 })
 
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    })
+
+    if (!found) {
+        res.status(4040).json('not found');
+    }
+})
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+    })
+
+    if (!found) {
+        res.status(400).json('not found');
+    }
+
+})
+
 app.post('/signin', (req, res) => {
     if (req.body.email === database.users[0].email &&
         req.body.password === database.users[0].password) {
-        res.json('success');
+        res.json(database.users[0]);
     } else {
         res.status(400).json('error logging in');
     }
@@ -41,18 +84,30 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
+    bcrypt.hash(password, null, null, function (err, hash) {
+        console.log(hash);
+    });
     database.users.push({
         id: '125',
         name: name,
         email: email,
-        password: password,
         entries: 0,
         joined: new Date()
     })
 
-    res.json(database.users[database.users.length-1])
+    res.json(database.users[database.users.length - 1])
 })
 
+
+
+
+// // Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function (err, res) {
+//     // res == true
+// });
+// bcrypt.compare("veggies", hash, function (err, res) {
+//     // res = false
+// });
 
 
 app.listen(3000, () => {
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,13 +1,10 @@
 import React from 'react';
 
-const Rank = () => {
+const Rank = ({name, entries}) => {
     return (
         <div>
             <div className='white f3'>
-                {'Benjamin, Your current rank is ...'}
-            </div>
-            <div className='white f3'>
-                {'#5'}
+                {`${name} , your current entry count is ${entries}`}
             </div>
         </div>
     )
