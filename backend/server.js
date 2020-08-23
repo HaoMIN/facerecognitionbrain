ebebf9bf4945ff1b4d62da61c40b7132ff45471f
Hao MIN <bennymin.1989@gmail.com>
2020-08-23 21:13:52 +0200

    Change Port for backend

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,7 @@ app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`app is running on port ${process.env.PORT}`);
+const appPort = process.env.PORT || 3000;
+app.listen(appPort, () => {
+    console.log(`app is running on port ${appPort}`);
 })
